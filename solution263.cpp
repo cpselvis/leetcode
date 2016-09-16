07af22eeaabee799be33178e4e1c6408d9169d72
cpselvis <cpselvis@gmail.com>
2016-09-16 23:03:51 +0800

    Ugly Number.

---
+++ solution263.cpp
@@ -0,0 +1,34 @@
+/**
+ * Ugly Number
+ *
+ * cpselvis (cpselvis@gmail.com)
+ * September 16th, 2016
+ */
+#include<iostream>
+
+using namespace std;
+
+class Solution {
+public:
+  bool isUgly(int num) {
+    int arr[3] = {2, 3, 5};
+
+    for (auto i : arr)
+    {
+      while (num % i == 0)
+      {
+	num /= i;
+      }
+    }
+    return num == 1;
+  }
+};
+
+int main(int argc, char **argv)
+{
+  Solution s;
+
+  cout << s.isUgly(6) << endl;
+  cout << s.isUgly(8) << endl;
+  cout << s.isUgly(14) << endl;
+}
